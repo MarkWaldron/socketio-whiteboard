4486be78f376bea233c5948f21152909d88fbe0d
MarkWaldron <markewaldron@gmail.com>
2015-12-01 17:30:14 -0500

    Added socket.io and the ability for users to share teh same whiteboard

--- browser/app.js
+++ browser/app.js
@@ -1,3 +1,18 @@
-whiteboard.on('draw', function(){
-  console.log(arguments);
+var socket = io(window.location.origin);
+
+socket.on('connect', function() {
+  console.log('I have made a connection with the server');
 });
+
+whiteboard.on('draw', function(start, end, color){
+  socket.emit('drawEvent', {start: start, end: end, color: color});
+});
+
+socket.on('otherUser', function(event){
+  console.log('app.JS otherUser');
+  var start = event.start;
+  var end = event.end;
+  var color = event.color;
+  console.log(whiteboard.draw);
+  whiteboard.draw(start, end, color);
+})
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var path = require('path');
-
 var http = require('http');
 var server = http.createServer();
-
 var express = require('express');
 var app = express();
+var socketio = require('socket.io');
 
 server.on('request', app);
+var io = socketio(server);
+
+io.on('connection', function (socket) {
+    // This function receives the newly connected socket.
+    // This function will be called for EACH browser that connects to our server.
+    console.log('A new client has connected!');
+    console.log(socket.id);
+
+
+    socket.on('drawEvent', function(event){
+      console.log('drawEvent');
+      socket.broadcast.emit('otherUser', event);
+    })
+
+    socket.on('disconnect', function(){
+      console.log("Client: " + socket.id + " has disconnected");
+    });
+});
 
 server.listen(1337, function () {
     console.log('The server is listening on port 1337!');
